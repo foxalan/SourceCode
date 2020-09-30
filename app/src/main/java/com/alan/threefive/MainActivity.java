ace23e151638c8f44062f9176d4b7c81992f7b7c
tangzijun <tangzijun@nbcb.com>
2020-09-30 09:58:20 +0800

    添加区分打包字段

--- app/src/main/java/com/alan/threefive/MainActivity.java
+++ app/src/main/java/com/alan/threefive/MainActivity.java
@@ -3,13 +3,24 @@ package com.alan.threefive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
 
+        if (BuildConfig.SKIP_MSG_CHECK_ENT) {
+            Log.e("ALAN","DEBUG");
+            textView.setText("DEBUG");
+        } else { Log.e("ALAN","RE");
+            textView.setText("DERE");
+        }
     }
 }
